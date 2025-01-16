89d35a92b24d5c9b8986ffd4b2d135fb50f2b0e9
Gustavo Leck <gustavoleck@hotmail.com>
2025-01-16 15:57:44 -0300

    feat(Criando pacotes)

--- CRM/Contacts/requests.go
+++ CRM/Contacts/requests.go
@@ -3,25 +3,25 @@ package ZohoContact
 import "fmt"
 
 //Cria conta no Zoho CRM
-func Create() any{
+func Create() any {
 	fmt.Println("Create Contact Api...")
 	return "Create Contact Api..."
 }
 
 //Consulta conta no Zoho CRM
-func Read() any{
+func Read() any {
 	fmt.Println("Read Contact Api...")
 	return "Read Contact Api..."
 }
 
 //Atualiza conta no Zoho CRM
-func Update() any{
+func Update() any {
 	fmt.Println("Update Contact Api...")
 	return "Update Contact Api..."
 }
 
 //Exclui conta no Zoho CRM
-func Delete() any{
+func Delete() any {
 	fmt.Println("Delete Contact Api...")
 	return "Delete Contact Api..."
 }
